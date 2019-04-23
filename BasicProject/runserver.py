c413aae2b8bc51da8ac49f8cec0b1ddd2b41dd60
Tom Newark <thomas.newark@phmc.com>
2019-04-23 09:56:36 -0500

    refactored to use views

---
+++ BasicProject/runserver.py
@@ -0,0 +1,17 @@
+"""
+This script runs the application using a development server.
+It contains the definition of routes and views for the application.
+"""
+
+import os
+from HelloFlask import app # import the code from HelloFlask/__init__.py
+
+if __name__ == '__main__':
+    HOST = os.environ.get("SERVER_HOST", 'localhost')
+    
+    try:
+        PORT = int(os.environ.get('SERVER_PORT', '5555'))
+    except ValueError:
+        PORT = 5555
+
+    app.run(HOST, PORT)
