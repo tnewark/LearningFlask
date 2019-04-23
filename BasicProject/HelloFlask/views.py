74e9f77c06ca39b9bde7162cadcac4905622b9e9
Tom Newark <thomas.newark@phmc.com>
2019-04-23 10:17:19 -0500

    refactored to use a template

--- BasicProject/HelloFlask/views.py
+++ BasicProject/HelloFlask/views.py
@@ -1,8 +1,15 @@
-from flask import Flask
+from datetime import datetime
+from flask import render_template
 from HelloFlask import app
 
 @app.route('/')
 @app.route('/home')
 def home():
-    return "Hello Flask!"
+    now = datetime.now()
+    formatted_now = now.strftime("%A, %d %B, %Y at %X ")
+    
+    return render_template(
+            "index.html",
+            message = "Hello, Flask!",
+            content = " on {}".format(formatted_now))
 
